1e4ce6af58d578f5898b620046ad2c3f26c4df4b
jcnpereira <casimiro@ipt.pt>
2025-03-27 11:49:11 +0000

    initial project

---
+++ PhotosErasmusApp/PhotosErasmusApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhotosErasmusApp.Data {
+   public class ApplicationDbContext: IdentityDbContext {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options) {
+      }
+   }
+}
